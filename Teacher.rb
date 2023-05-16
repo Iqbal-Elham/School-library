a2ac1248ed3bff4674e25ed6c251859a6e2ad0ff
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-16 23:26:34 +0430

    Fix the linter errors

--- Teacher.rb
+++ Teacher.rb
@@ -1,13 +1,12 @@
-require './Person'
+require './person'
 
 class Teacher < Person
+  def initialize(specialization, age, name, parent_permission)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def initialize(specialization, age, name = 'Unknown', parent_permission = true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
-
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
