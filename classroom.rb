25f0cd738dd310b6ab941d38a559c3712cae066d
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-18 21:35:49 +0430

    Add add_student method to classroom

--- classroom.rb
+++ classroom.rb
@@ -1,8 +1,14 @@
 class Classroom 
     def initialize(label)
         @label = label
+        @students = []
     end
 
     attr_accessor :label
+    attr_accessor :students
 
+    def add_student(student)
+        @students << student
+        student.classroom = self
+    end
 end
