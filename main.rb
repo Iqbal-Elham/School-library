023307e680492b38c421c16d911b956c2b092630
iqbal-elham <iqbal.elham7@gmail.com>
2023-05-19 17:53:30 +0430

    Add switch method to main.rb

--- main.rb
+++ main.rb
@@ -1,28 +1,41 @@
 require './app'
 
-def main
-    app = App.new
-    puts "Welcome to School Library App!\n"
-    menu = "Please choose an option by entering a number:\n1 - List all books\n2 - List all people \n3 - Create a person\n4 - Create a book\n5 - Create a rental\n6 - List all rentals for a given person ID\n7 - Exit"
-    get = 0
-    while get != 7
-        puts menu
-        get = gets.chomp.to_i
-        case get
-        when 1
-            app.list_books
-        when 2
-            app.list_people
-        when 3
-            app.create_person
-        when 4
-            app.create_book
-        when 5 
-            app.create_rental
-        when 6
-            app.list_rentals
-        end
-    end
-    puts "Thank you"
+puts "Welcome to School Library App!\n\n"
+MENU = "Please choose an option by entering a number:\n\
+1 - List all books\n\
+2 - List all people \n\
+3 - Create a person\n\
+4 - Create a book\n\
+5 - Create a rental\n\
+6 - List all rentals for a given person ID\n\
+7 - Exit".freeze
+APP = App.new
+
+def switch(get)
+  case get
+  when 1
+    APP.list_books
+  when 2
+    APP.list_people
+  when 3
+    APP.create_person
+  when 4
+    APP.create_book
+  when 5
+    APP.create_rental
+  when 6
+    APP.list_rentals
+  end
 end
+
+def main
+  get = 0
+  while get != 7 and get < 7
+    puts MENU
+    get = gets.chomp.to_i
+    switch(get)
+  end
+  puts 'Thank you for using this app!'
+end
+
 main
